2453bdbaae94a1486b4eebb048cd0f6d2b6e8e2a
Tashydaije <mbuguanatasha01@gmail.com>
2024-05-31 02:37:44 +0000

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,4 +10,15 @@ def pascal_triangle(n):
     if n <= 0:
         return [];
 
+    """Initialize first row with 1"""
+    triangle = [[1]]
 
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            """add elements from the previous row"""
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
