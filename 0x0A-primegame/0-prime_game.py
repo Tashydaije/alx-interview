90744578b215eec5de14c0f62870b4c030599493
Tashydaije <mbuguanatasha01@gmail.com>
2024-08-07 04:20:22 -0700

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,26 +20,34 @@ def isWinner(x, nums):
     """handles game simulation and winner determination"""
     if not nums or x <= 0:
         return None
-    
+
     max_num = max(nums)
     primes = sieve(max_num)
-    
-    # DP array to count the number of prime removals up to each number
-    prime_counts = [0] * (max_num + 1)
-    
-    for prime in primes:
-        for multiple in range(prime, max_num + 1, prime):
-            prime_counts[multiple] += 1
-    
+
+    # Create a set of primes for quick lookup
+    prime_set = set(primes)
+
+    def count_prime_moves(n):
+        """Count the number of moves possible for a given n"""
+        moves = 0
+        visited = set()
+        for num in range(2, n + 1):
+            if num not in visited and num in prime_set:
+                moves += 1
+                for multiple in range(num, n + 1, num):
+                    visited.add(multiple)
+        return moves
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
-        if prime_counts[n] % 2 == 1:
+        moves = count_prime_moves(n)
+        if moves % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
